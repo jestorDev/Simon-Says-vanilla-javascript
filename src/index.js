59000356fe3a8a47d2285861d03faf9fad39cc1c
Jesus <jtorrejonl@uni.pe>
2020-06-13 16:33:03 -0500

    Finished for presentation

--- src/index.js
+++ src/index.js
@@ -1,56 +1,51 @@
 class SimonSays {
-    constructor() {
-        this.hits = 0;
-        this.rdmSelected = [];
-        this.buttons = document.getElementById("buttonsContainer").children;
-        this.addBtnEvents();
-        this.game();
+  constructor() {
+    this.selectedNumbers = [];
+    this.buttons = document.getElementById("buttonsContainer").children;
+    this.addButtonEvents();
+    this.game();
+  }
+  game() {
+    this.hits = 0;
+    //new random value to queue
+    this.selectedNumbers.push(this.randomButtonNumber());
+    //show actual sequence
+    this.showSelectedNumbers();
+  }
+  randomButtonNumber() {
+    return Math.floor(Math.random() * this.buttons.length);
+  }
+  showSelectedNumbers() {
+    this.selectedNumbers.forEach((number, index) => {
+      setTimeout(() => this.makeBtnGlow(number), 900 * index);
+    });
+  }
+  makeBtnGlow(number) {
+    this.buttons[number].classList.add("glow"); //turn on
+    setTimeout(() => this.buttons[number].classList.remove("glow"), 700); //turn off after 700
+  }
+  addButtonEvents() {
+    for (let i = 0; i < this.buttons.length; i++)
+      this.buttons[i].addEventListener("click", (event) => {
+        // clicked == ?
+        if (i == this.selectedNumbers[this.hits]) this.hits++;
+        else this.userLose();
+        // player arrive to last element
+        // -> new game (same sequence + one)
+        if (this.hits == this.selectedNumbers.length) this.game();
+      });
+  }
+  userLose() {
+    for (let i = 0; i < this.buttons.length; i++) {
+      this.buttons[i].classList.add("dark");
     }
-    game() {
-        this.showrdmSelectedSequence();
-    }
-    showrdmSelectedSequence() {
-        this.rdmSelected.push(this.rdmButton());
-        for (let i = 0; i < this.rdmSelected.length; i++)
-            setTimeout(() => this.makeBtnGlow(this.rdmSelected[i]), 800 * i);
-    }
-    makeBtnGlow(number) {
-        let btn = this.buttons.item(number);
-        btn.classList.add('glow');
-        setTimeout(() => btn.classList.remove('glow'), 500)
-        console.log(number);
-    }
-    rdmButton() {
-        return (Math.floor(Math.random() * 4));
-    }
-    addBtnEvents() {
-        for (let i = 0; i < 4; i++)
-            this.buttons.item(i).addEventListener('click', (event) => {
-                if (i == this.rdmSelected[this.hits])this.hits++;
-                else {
-                    console.log("u lose")
-                    this.userLose();
-                }
-
-                if (this.hits == this.rdmSelected.length){
-                    this.hits = 0;
-                    this.game();
-                }
-            });
-    }
-    isHit(number) {
-        return number == this.rdmSelected[this.rdmSelected.length - this.hits - 1]
-    }
-    userLose(){
-        for (let i = 0; i < 4; i++) {
-            this.buttons.item(i).classList.add('dark');            
-        }
-    }
+  }
 }
-function randomColors() {
-    let randomHue = Math.floor(Math.random() * 360);
-    document.documentElement.style.setProperty('--hueNumber', randomHue);
 
+function randomColors() {
+  //Random hue 
+  let randomHue = Math.floor(Math.random() * 360);
+  document.documentElement.style.setProperty("--hueNumber", randomHue);
 }
 randomColors();
 let a = new SimonSays();
